1ad07afb29ed77df88b03d8152fadcea17f6dd82
Gustavo Dourado <gustavodourado23@gmail.com>
2025-08-13 15:45:48 -0300

    create function paintBlack and init construction of createNewBoard with user choice

--- script.js
+++ script.js
@@ -2,13 +2,22 @@
 const board = document.querySelector(".board-container");
 const defineSquares = document.querySelector("#define-squares");
 
+//Constantes
 const INITIAL_SQUARES = 16;
+const WIDTH = board.clientWidth;
 
 function createBoard(){
     createSquares(INITIAL_SQUARES);
 }
 
+function clearBoard(){
+    //clear
+    
+}
+
 function createSquares(squarePerLine){
+
+    let widthSquare = WIDTH/squarePerLine;
 
     let totalSquares = squarePerLine ** 2;
 
@@ -16,8 +25,30 @@ function createSquares(squarePerLine){
         const square = document.createElement("div");
         square.setAttribute("class", "square");
         square.setAttribute("id", `square-${id}`);
+        square.style.width = widthSquare;
+        square.style.height = widthSquare;
         board.appendChild(square);
     }
 }
+
+function createNewBoard(){
+    const userDefineNumber = prompt("Escolha como você quer montar o quadro: \n Digite o número de quadrados por linha (1 a 100)")
+
+    if(!userDefineNumber || userDefineNumber === 0)
+        alert("Escolha inválida, tente um número de 1 a 100")
+
+    createSquares(userDefineNumber);
+}
+
+defineSquares.addEventListener('click', createNewBoard);
+board.addEventListener('mouseover', (e) => paintBlack(e.target));
+
+function paintBlack(square){
+    if (!square.classList.contains("square"))
+        return;
+
+    square.style.backgroundColor = "black";
+}
+
 
 createBoard();
